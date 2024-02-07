63565afca8e3b93919cc21716964b3ef1f492265
Anton SHEPILOV <ashepilov@linagora.com>
2024-02-07 17:02:37 +0100

    🔒Upgrade all dependencies to fix security warning

--- tdrive/backend/node/src/core/platform/services/auth/web/jwt.ts
+++ tdrive/backend/node/src/core/platform/services/auth/web/jwt.ts
@@ -33,15 +33,15 @@ const jwtPlugin: FastifyPluginCallback = (fastify, _opts, next) => {
     request.log.debug(`Authenticated as user ${request.currentUser.id}`);
   };
 
-  fastify.decorate("authenticate", async (request: FastifyRequest) => {
+  fastify.decorateRequest("authenticate", async (request: FastifyRequest) => {
     try {
       await authenticate(request);
     } catch (err) {
-      throw fastify.httpErrors.unauthorized(`Bad credentials ${JSON.stringify(err)}`);
+      throw fastify.httpErrors.unauthorized;
     }
   });
 
-  fastify.decorate("authenticateOptional", async (request: FastifyRequest) => {
+  fastify.decorateRequest("authenticateOptional", async (request: FastifyRequest) => {
     try {
       await authenticate(request);
     } catch (err) {}
