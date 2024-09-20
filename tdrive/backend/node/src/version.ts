bf65b35ed37161aa23217772cacc193a204dab7a
Monta <monta@HP-ProBook-445-14-inch-G9-Notebook-PC-505aadfc.localdomain>
2024-09-20 10:40:18 +0100

    🔖 Release Version 1.0.4-rc1

--- tdrive/backend/node/src/version.ts
+++ tdrive/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "1.0.3",
+  current: /* @VERSION_DETAIL */ "1.0.4",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "1.0.3",
-    mobile: /* @MIN_VERSION_MOBILE */ "1.0.3",
+    web: /* @MIN_VERSION_WEB */ "1.0.4",
+    mobile: /* @MIN_VERSION_MOBILE */ "1.0.4",
   },
 };
