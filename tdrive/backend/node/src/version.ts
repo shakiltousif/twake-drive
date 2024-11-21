f272db23f0337d48271357e42fbfa6d54932f868
Eric Doughty-Papassideris <edoughtypapassideris@linagora.com>
2024-11-21 11:24:38 +0100

    🔖 Release Version 1.0.5-rc5

--- tdrive/backend/node/src/version.ts
+++ tdrive/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "v1.0.5-rc4",
+  current: /* @VERSION_DETAIL */ "v1.0.5-rc5",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "v1.0.5-rc4",
-    mobile: /* @MIN_VERSION_MOBILE */ "v1.0.5-rc4",
+    web: /* @MIN_VERSION_WEB */ "v1.0.5-rc5",
+    mobile: /* @MIN_VERSION_MOBILE */ "v1.0.5-rc5",
   },
 };
