7c0f3597599a098cae09bdc0e98179b5854d5a2c
Eric Doughty-Papassideris <edoughtypapassideris@linagora.com>
2024-12-08 22:53:43 +0100

    ✨ back: adding storage service diagnostics (#762)

--- tdrive/backend/node/src/core/platform/services/diagnostics/providers/index.ts
+++ tdrive/backend/node/src/core/platform/services/diagnostics/providers/index.ts
@@ -1,9 +1,11 @@
-import registerDBPingProvider from "./database-service";
+import registerDBServiceProvider from "./database-service";
+import registerStorageServiceProvider from "./storage-service";
 import registerPlatformProvider from "./platform-started";
 import registerProcessProvider from "./process";
 
 export default () => {
-  registerDBPingProvider();
+  registerDBServiceProvider();
+  registerStorageServiceProvider();
   registerPlatformProvider();
   registerProcessProvider();
 };
