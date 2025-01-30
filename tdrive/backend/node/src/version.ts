14d42d85c6ee9a6f5c7141bf9623fe590ac23f51
Anton SHEPILOV <ashepilov@linagora.com>
2025-01-30 19:02:52 +0100

    🔖 Release Version 1.0.6-rc5

--- tdrive/backend/node/src/version.ts
+++ tdrive/backend/node/src/version.ts
@@ -1,7 +1,7 @@
 export default {
-  current: /* @VERSION_DETAIL */ "1.0.6-rc4",
+  current: /* @VERSION_DETAIL */ "1.0.6-rc5",
   minimal: {
-    web: /* @MIN_VERSION_WEB */ "1.0.6-rc4",
-    mobile: /* @MIN_VERSION_MOBILE */ "1.0.6-rc4",
+    web: /* @MIN_VERSION_WEB */ "1.0.6-rc3",
+    mobile: /* @MIN_VERSION_MOBILE */ "1.0.6-rc3",
   },
 };
